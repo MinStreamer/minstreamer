6bdd98ceaefdec5c616fff075a7e3152088b10b6
spencer burke <info.spencer.burke@gmail.com>
2025-05-24 23:07:09 -0700

    Add the files for the new development environment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+</script>
+
+<nav>
+    <p>Minstreamer</p>
+    <a href='/login'>Login</a>
+    <a href='/register'>Register</a>
+</nav>
+
+<section class='project-description'>
+<p>
+MinStreamer is a simple project dedicated to demystifying video streaming in the browser, and providing a template for fullstack fastAPI applications.
+</p>
+</section>
+
+<section class='project-stack'>
+<p>Tech Stack:</p>
+<ul>
+    <li>sveltekit</li>
+    <li>fastAPI</li>
+    <li>postgresql</li>
+    <li>redis</li>
+    <li>ory kratos</li>
+</ul>
+</section>
+
+<section class='project-hosting'>
+<p>Hosting:</p>
+<ul>
+    <li>frontend: cloudflare</li>
+    <li>backend: railway</li>
+</ul>
+</section>
